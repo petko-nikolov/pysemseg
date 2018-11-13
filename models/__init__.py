cea06bf44f1557622bcb17e19798035d024b5bb4
Petko Nikolov <ov32m1nd@gmail.com>
2018-11-13 11:32:53 +0200

    Delete simple convolution

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .simple_conv import SimpleConvNet
 from .unet import UNet
 from .fcn import VGGFCN32, VGGFCN16, VGGFCN8
 from .densenet import FCDenseNet56, FCDenseNet67, FCDenseNet103
+from .deeplab import DeepLabv3
