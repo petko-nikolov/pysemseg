5aa4639bbcb2c50f9a71081439f2b853149051ab
Petko Nikolov <ov32m1nd@gmail.com>
2018-11-27 13:22:36 +0200

    better console logging format

--- pysemseg/loggers/console_logger.py
+++ pysemseg/loggers/console_logger.py
@@ -16,11 +16,12 @@ class ConsoleLogger():
         self.log_file.close()
 
     def log(self, index, epoch, loader, data, metrics, mode='Train'):
-        print("".join(40 * ['-']))
-        print('{} Epoch: {} [{}/{} ({:.0f}%)]'.format(
+        metric_str = ", ".join([
+            '{}:{:.6f}'.format(k, v) for k, v in metrics.items()
+        ])
+        print('{} Epoch: {} [{}/{} ({:.0f}%)] [{}]'.format(
             mode, epoch, index * len(data), len(loader.dataset),
-            100. * index / len(loader)))
-        pprint.pprint(metrics)
+            100. * index / len(loader), metric_str))
         if self.log_file is not None:
             log_data = {**metrics, 'epoch': epoch, 'step': index, "mode": mode}
             self.log_file.write(json.dumps(log_data) + '\n')
